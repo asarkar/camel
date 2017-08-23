b182650fb1561868ef1f733c9911606657e0d2ee
Abhijit Sarkar <abhijit.sarkar@nuance.com>
2017-08-22 18:01:17 -0700

    Minor refactoring

--- docker-registry-listener/src/main/kotlin/org/abhijitsarkar/camel/util/FileUtil.kt
+++ docker-registry-listener/src/main/kotlin/org/abhijitsarkar/camel/util/FileUtil.kt
@@ -3,35 +3,52 @@ package org.abhijitsarkar.camel.util
 import org.abhijitsarkar.camel.model.Event
 import org.yaml.snakeyaml.Yaml
 import java.io.File
+import java.util.Objects
 
 /**
  * @author Abhijit Sarkar
  */
+
+data class DockerImage(private val prefix: String, val name: String, val tag: String) {
+    override fun toString(): String {
+        return "$prefix$name:$tag"
+    }
+}
+
+private fun String.toDockerImage(): DockerImage? {
+    val groups = "^(.+)(?<=/)(.+):(.+)\$".toRegex()
+            .matchEntire(this as CharSequence)
+            ?.groupValues
+
+    return if (groups != null && groups.size >= 4) {
+        DockerImage(groups[1], groups[2], groups[3])
+    } else null
+}
+
 fun File.updateIfNecessary(events: List<Event>): Pair<Map<String, List<String>>, Map<String, String>> =
         this.bufferedReader().use {
             @Suppress("UNCHECKED_CAST")
             val dockerImages = Yaml().load(it) as MutableMap<String, String>
-            var imagesUpdated = mutableMapOf<String, List<String>>()
+            val imagesUpdated = mutableMapOf<String, List<String>>()
 
-            dockerImages.keys.forEach { image ->
-                val v = dockerImages[image]
-                val groups = "^(.+)(?<=/)(.+):(.+)\$".toRegex()
-                        .matchEntire(v as CharSequence)
-                        ?.groupValues
-
-                if (groups != null && groups.size >= 4) {
-                    val repository = groups[2]
-                    val tag = groups[3]
-
-                    val event = events
-                            .find { e -> e.target.repository.endsWith(repository) && e.target.tag != tag }
-
-                    if (event != null) {
-                        dockerImages[image] = "${groups[1]}${groups[2]}:${event.target.tag}"
-                        imagesUpdated.merge(event.id, listOf(image), { k1, k2 -> k1 + k2 })
+            dockerImages
+                    .mapValues { (_, v) -> v.toDockerImage() }
+                    .filter(Objects::nonNull)
+                    .map { (k, v) ->
+                        events
+                                .find {
+                                    it.target.repository.endsWith(v!!.name)
+                                            && it.target.tag != v.tag
+                                }
+                                ?.let {
+                                    Triple(it.id, k, v!!.copy(tag = it.target.tag))
+                                }
                     }
-                }
-            }
+                    .filter(Objects::nonNull)
+                    .forEach {
+                        dockerImages[it!!.second] = it.third.toString()
+                        imagesUpdated.merge(it.first, listOf(it.second), { k1, k2 -> k1 + k2 })
+                    }
 
             // no first-class support for mutable to immutable map
             imagesUpdated.to(dockerImages)
